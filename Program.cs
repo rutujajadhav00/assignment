b59306130b5a52db85b1bfd7c652137d8aa8ad22
rutujajadhav00 <rutujajadhav413512@gmail.com>
2025-05-09 16:36:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] daysOfWeek = {
+            "Sunday", "Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday"};
+
+        Console.WriteLine("Days of the week:");
+        foreach (string day in daysOfWeek)
+        {
+            Console.WriteLine(day);
+        }
+    }
+}
+
+
+
+
+
+
